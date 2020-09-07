8cd924df31816749a8ef0398ba9f786823c8bc8e
Filip Mihajlovski <fitz993m@gmail.com>
2020-09-07 15:56:19 +0200

    1.24. Updated app module

--- acc-back/src/app/app.module.ts
+++ acc-back/src/app/app.module.ts
@@ -14,16 +14,28 @@ import { TransactionsController } from '../transactions/transactions.controller'
 import { TransactionsModule } from '../transactions/transactions.module';
 import { TransactionsService } from '../transactions/transactions.service';
 import { transactionsProviders } from '../transactions/transactions.providers';
+// Vendor
+import { VendorModule } from 'src/vendor/vendor.module';
+import { VendorController } from 'src/vendor/vendor.controller';
+import { VendorService } from 'src/vendor/vendor.service';
+import { vendorProviders } from 'src/vendor/vendor.providers';
 
 @Module({
-  imports: [DatabaseModule, AccountsModule, TransactionsModule],
-  controllers: [AppController, AccountsController, TransactionsController],
+  imports: [DatabaseModule, AccountsModule, TransactionsModule, VendorModule],
+  controllers: [
+    AppController,
+    AccountsController,
+    TransactionsController,
+    VendorController,
+  ],
   providers: [
     AppService,
     AccountsService,
     ...accountProviders,
     TransactionsService,
     ...transactionsProviders,
+    VendorService,
+    ...vendorProviders,
   ],
 })
 export class AppModule {}
