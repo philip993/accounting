b2af996719bd8a603febbf23cf7158e3d7cfc16f
Filip FICO <fitz993m@gmail.com>
2020-09-09 00:12:49 +0200

    1.27. Added Invoice to App

--- acc-back/src/app/app.module.ts
+++ acc-back/src/app/app.module.ts
@@ -19,14 +19,26 @@ import { VendorModule } from 'src/vendor/vendor.module';
 import { VendorController } from 'src/vendor/vendor.controller';
 import { VendorService } from 'src/vendor/vendor.service';
 import { vendorProviders } from 'src/vendor/vendor.providers';
+// Invoice
+import { InvoiceController } from 'src/invoice/invoice.controller';
+import { InvoiceService } from 'src/invoice/invoice.service';
+import { invoiceProviders } from 'src/invoice/invoice.providers';
+import { InvoiceModule } from 'src/invoice/invoice.module';
 
 @Module({
-  imports: [DatabaseModule, AccountsModule, TransactionsModule, VendorModule],
+  imports: [
+    DatabaseModule,
+    AccountsModule,
+    TransactionsModule,
+    VendorModule,
+    InvoiceModule,
+  ],
   controllers: [
     AppController,
     AccountsController,
     TransactionsController,
     VendorController,
+    InvoiceController,
   ],
   providers: [
     AppService,
@@ -36,6 +48,8 @@ import { vendorProviders } from 'src/vendor/vendor.providers';
     ...transactionsProviders,
     VendorService,
     ...vendorProviders,
+    InvoiceService,
+    ...invoiceProviders,
   ],
 })
 export class AppModule {}
