8ecc309ff41170de77721379a8604b2e258c3d17
Filip Mihajlovski <fitz993m@gmail.com>
2020-10-01 15:30:05 +0200

    1.82. Home page - v1

--- acc-front/src/components/Home/Home.js
+++ acc-front/src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+// React Router Dom
+import { Link } from 'react-router-dom';
+// Style
+import './HomeStyle.scss';
+
 const Home = () => {
   return (
     <div className="home">
       <h1>Accounting</h1>
+      <Link className="link" to="/general">
+        General Ledgder
+      </Link>
+      <Link className="link" to="/receivables">
+        Receivables
+      </Link>
+      <Link className="link" to="/payables">
+        Payables
+      </Link>
+      <Link className="link" to="/humanresources">
+        Human Resources
+      </Link>
     </div>
   );
 };
