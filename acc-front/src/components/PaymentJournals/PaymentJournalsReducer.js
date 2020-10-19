f443ec17944092c428e5521d4bb135ad3de40bb4
Filip Mihajlovski <fitz993m@gmail.com>
2020-10-19 15:45:58 +0200

    3.15. Payment Journals components

---
+++ acc-front/src/components/PaymentJournals/PaymentJournalsReducer.js
@@ -0,0 +1,36 @@
+import {
+  SUCCESS_GET_PAYMENT_JOURNALS,
+  FAILURE_GET_PAYMENT_JOURNALS,
+  GET_NUMBER_OF_PAYMENT_JOURNAL,
+} from './PaymentJournalsActionTypes';
+
+const initialState = {
+  allPaymentJournals: [],
+  allPaymentJournalsError: null,
+  paymentJournalLength: 0,
+};
+
+export const PaymentJournalsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SUCCESS_GET_PAYMENT_JOURNALS:
+      return {
+        ...state,
+        allPaymentJournals: action.payload,
+        allPaymentJournalsError: false,
+        paymentJournalLength: state.allPaymentJournals.length,
+      };
+    case FAILURE_GET_PAYMENT_JOURNALS:
+      return {
+        ...state,
+        allPaymentJournals: null,
+        allPaymentJournalsError: true,
+      };
+    // case GET_NUMBER_OF_PAYMENT_JOURNAL:
+    //   return {
+    //     ...state,
+    //     paymentJournalLength: state.allPaymentJournals.length,
+    //   };
+    default:
+      return state;
+  }
+};
